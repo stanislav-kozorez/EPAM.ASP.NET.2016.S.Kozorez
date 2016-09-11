cc5ba57f46ef37b33437da20aa58a2d34b9ff2be
_shark <stanislav.kozorez@gmail.com>
2016-09-12 01:17:59 +0300

    add person model binder

---
+++ Models/Models/Infrastructure/PersonModelBinder.cs
@@ -0,0 +1,42 @@
+﻿using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Models.Infrastructure
+{
+    public class PersonModelBinder : IModelBinder
+    {
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var model = bindingContext.Model as Person ?? new Person();
+
+            model.FirstName = GetValue(bindingContext, "FirstName");
+            model.LastName = GetValue(bindingContext, "LastName");
+
+            return model;
+        }
+
+        private string GetValue(ModelBindingContext context, string name)
+        {
+            var valueProvider = context.ValueProvider;
+            var valueProviderResult = valueProvider.GetValue(name);
+
+            switch (name)
+            {
+                case "FirstName":
+                    if (valueProviderResult == null || valueProviderResult.AttemptedValue.Length < 2)
+                        return "John";
+                    break;
+                case "LastName":
+                    if (valueProviderResult == null || valueProviderResult.AttemptedValue.Length < 3)
+                        return "Smith";
+                    break;
+            }
+
+            return valueProviderResult.AttemptedValue;
+        }
+    }
+}
